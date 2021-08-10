c601ca154ec268ca98c77a28367b9d85f72cc4eb
matt <mpp21x@gmail.com>
2021-08-10 11:44:56 +0800

    (feature)bootstrap pagination table

--- src/app/feature/table/table.module.ts
+++ src/app/feature/table/table.module.ts
@@ -4,11 +4,13 @@ import {CommonModule} from '@angular/common';
 import {TableRoutingModule} from './table-routing.module';
 import {TableComponent} from './pages/table/table.component';
 import {BootstrapTableModule} from 'ng-matt-tw-library';
+import { PaginationTableComponent } from './pages/pagination-table/pagination-table.component';
 
 
 @NgModule({
   declarations: [
-    TableComponent
+    TableComponent,
+    PaginationTableComponent
   ],
   imports: [
     CommonModule,
